c8341eeaa4d1210953055090c858a7bf0bdf305e
Ben Stone <benjamin.h.stone@gmail.com>
2020-03-06 10:25:18 -0500

    fix variable naming typo

--- dup.js
+++ dup.js
@@ -1,7 +1,7 @@
 //Write a function that returns all the duplicate numbers in an array
 
 function find_duplicate(arra1) {
-  let vistiedVals = new Set()
+  let visitedVals = new Set()
   let dupes = []
   for (let val of arra1) {
     if (visitedVals.has(val)) {
