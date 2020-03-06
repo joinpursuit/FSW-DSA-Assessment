01136c0ddaab43517ae5ff7306ddf767c15bd037
secretesting <61860067+secretesting@users.noreply.github.com>
2020-03-06 00:47:13 -0500

    solution_to_dups

--- dup.js
+++ dup.js
@@ -1,7 +1,22 @@
 //Write a function that returns all the duplicate numbers in an array
 
 function find_duplicate(arra1) {
+  var object = {};
+  var result = [];
 
+  arra1.forEach(function (item) {
+    if(!object[item])
+        object[item] = 0;
+      object[item] += 1;
+  })
+
+  for (var prop in object) {
+     if(object[prop] >= 2) {
+         result.push(Number(prop));
+     }
   }
+
+  return result; 
+}
 
   module.exports = find_duplicate;
