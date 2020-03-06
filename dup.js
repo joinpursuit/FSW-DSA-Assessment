09d00e1e992045223c517c35f05a885ca991008f
Ben Stone <benjamin.h.stone@gmail.com>
2020-03-06 10:24:10 -0500

    add dupes solution

--- dup.js
+++ dup.js
@@ -1,7 +1,16 @@
 //Write a function that returns all the duplicate numbers in an array
 
 function find_duplicate(arra1) {
- 
+  let vistiedVals = new Set()
+  let dupes = []
+  for (let val of arra1) {
+    if (visitedVals.has(val)) {
+      dupes.push(val)
+    } else {
+      visitedVals.add(val)
+    }
   }
+  return dupes
+}
 
-  module.exports = find_duplicate;
+module.exports = find_duplicate;
