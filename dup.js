e666fec33415785168024d57d7d65319117315de
lynksdomain <alanjholguin@gmail.com>
2020-03-06 17:20:37 -0500

    okay back to testing as a fellow

--- dup.js
+++ dup.js
@@ -1,12 +1,12 @@
 //Write a function that returns all the duplicate numbers in an array
 
 function find_duplicate(arra1) {
-    return [2];
-  };
+
+};
 
 
   function sum(a,b) {
-   return a + b;
+
   };
 
   module.exports = {find_duplicate, sum};
